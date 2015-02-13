16be55d017b8f1545249ca285c2927bc5f9665ef
Adrien LAUER <adrien.lauer@mpsa.com>
2015-02-13 09:59:33 +0000

    Changed commit msg

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -6,6 +6,6 @@
 	git config user.name "Travis-CI"
 	git config user.email "travis@seedstack.org"
 	git add .
-	git commit -m "Prepared for Github Pages"
+	git commit -m "Built for seedstack.github.org"
 	git push --force --quiet "https://${GITHUB_TOKEN}@github.com/seedstack/seedstack.github.io" master:master > /dev/null 2>&1
 )
