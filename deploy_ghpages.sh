0cc30d0e1fb8e3163af7181dcb8b180c180397ef
Adrien LAUER <adrien.lauer@gmail.com>
2015-05-16 11:21:00 +0200

    Use force with lease for gh pages push

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -7,5 +7,5 @@
 	git config user.email "travis@seedstack.org"
 	git add .
 	git commit -m "Built for seedstack.github.org"
-	git push --force --quiet "https://${GITHUB_TOKEN}@github.com/seedstack/seedstack.github.io" master:master > /dev/null 2>&1
+	git push --force-with-lease --quiet "https://${GITHUB_TOKEN}@github.com/seedstack/seedstack.github.io" master:master > /dev/null 2>&1
 )
