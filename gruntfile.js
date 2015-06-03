0902208d614d1277d3a155037b201befb455b6bc
Adrien LAUER <adrien.lauer@gmail.com>
2015-06-03 23:45:18 +0200

    Add gruntfile to create offline lunr index

---
+++ gruntfile.js
@@ -0,0 +1,83 @@
+var toml = require("toml");
+var yaml = require("yamljs");
+var S = require("string");
+
+var CONTENT_PATH_PREFIX = "content";
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("lunr-index", function() {
+
+        grunt.log.writeln("Building pages index");
+
+        var indexPages = function() {
+            var pagesIndex = [];
+            grunt.file.recurse(CONTENT_PATH_PREFIX, function(abspath, rootdir, subdir, filename) {
+                pagesIndex.push(processFile(abspath, filename));
+            });
+
+            return pagesIndex;
+        };
+
+        var processFile = function(abspath, filename) {
+            var pageIndex;
+
+            if (S(filename).endsWith(".html")) {
+                pageIndex = processHTMLFile(abspath, filename);
+            } else if (S(filename).endsWith(".md")) {
+                pageIndex = processMDFile(abspath, filename);
+            }
+
+            return pageIndex;
+        };
+
+        var processHTMLFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageName = S(filename).chompRight(".html").s;
+            var href = S(abspath)
+                .chompLeft(CONTENT_PATH_PREFIX).s;
+            return {
+                title: pageName,
+                href: href,
+                content: S(content).trim().stripTags().stripPunctuation().s
+            };
+        };
+
+        var processMDFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageIndex;
+            // First separate the Front Matter from the content and parse it
+            var frontMatter;
+            try {
+            	var yamlContent = content.split("---");
+                frontMatter = yaml.parse(yamlContent[1].trim());
+            } catch (e1) {
+		try {
+            	    var tomlContent = content.split("+++");
+	            frontMatter = toml.parse(tomlContent[1].trim())
+		} catch (e2) {
+		    grunt.fail.warn("Could not find a YAML or TOML header in " + abspath);
+		}
+            }
+
+            var href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(".md").s;
+            // href for index.md files stops at the folder name
+            if (filename === "index.md") {
+                href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(filename).s;
+            }
+
+            // Build Lunr index for this page
+            pageIndex = {
+                title: frontMatter.title,
+                tags: frontMatter.tags,
+                href: href,
+                content: S(content[2]).trim().stripTags().stripPunctuation().s
+            };
+
+            return pageIndex;
+        };
+
+        grunt.file.write("public/lunr-index.json", JSON.stringify(indexPages()));
+        grunt.log.ok("Index built");
+    });
+};
